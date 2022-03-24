d619f4e1fb032cb8acd583c0e96f9ce501a3a8d7
ZainAshfaq453 <80332757+ZainAshfaq453@users.noreply.github.com>
2022-03-24 13:56:44 +0000

    Edits

--- screens/LoginUserAccount.js
+++ screens/LoginUserAccount.js
@@ -90,42 +90,5 @@ class UserLogInScreen extends Component {
     );
   }
 }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "top",
-//     paddingTop: 20,
-//     borderWidth: 0,
-//     borderColor: "black",
-//   },
-//   instructions: {
-//     color: "#888",
-//     fontSize: 18,
-//     marginHorizontal: 15,
-//     marginBottom: 10,
-//   },
-//   text: {
-//     fontSize: 15,
-//     fontWeight: "bold",
-//     textAlign: "center",
-//     color: "#4267B2",
-//     paddingTop: 30,
-//   },
-//   button: {
-//     backgroundColor: "#4267B2",
-//     padding: 20,
-//     borderRadius: 5,
-//   },
-//   buttonText: {
-//     fontSize: 20,
-//     color: "#fff",
-//   },
-//   input: {
-//     fontSize: 20,
-//     color: "#fff",
-//   },
-// });
 
 export default UserLogInScreen;
